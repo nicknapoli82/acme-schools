83481a0108e4d9245803b55cf4334c0c13232bf9
Justin Cook <justin.cook.dev@gmail.com>
2019-07-11 10:19:00 -1000

    Fixed syncAndSeed in db.js

--- server/db.js
+++ server/db.js
@@ -1,8 +1,12 @@
 const Sequelize = require('sequelize');
 const fs = require('fs');
+const path = require('path');
 const faker = require('faker');
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme-school', { logging: false });
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/acme-school',
+  { logging: false }
+);
 
 // Seriously thought about removing timestamps from these models, but for now I think it should be
 // fine. Now worries here.
@@ -10,86 +14,93 @@ const Student = db.define('student', {
   id: {
     type: Sequelize.UUID,
     defaultValue: Sequelize.UUIDV4,
-    primaryKey: true
+    primaryKey: true,
   },
   firstName: {
     type: Sequelize.STRING,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
   },
   lastName: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
   },
   email: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      isEmail: true
-    }
+      isEmail: true,
+    },
   },
   GPA: {
     type: Sequelize.FLOAT,
     allowNull: true,
     validate: {
-      isNumeric: true
-    }
-  }
+      isNumeric: true,
+    },
+  },
 });
 
 const School = db.define('school', {
   id: {
     type: Sequelize.UUID,
     defaultValue: Sequelize.UUIDV4,
-    primaryKey: true
+    primaryKey: true,
   },
   name: {
     type: Sequelize.STRING,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
   },
   imageLocation: {
-    type: Sequelize.STRING
-  }
+    type: Sequelize.STRING,
+  },
 });
 
 Student.belongsTo(School);
 School.hasMany(Student);
 
 async function syncAndSeed() {
-  try{
-    await db.sync({force: true});
-    await fs.readFile('./seedData/schools.json', async (err, result)=> {
-      const data = JSON.parse(result);
-      const schools = await Promise.all(
-        data.map((school)=>
-                 School.create({name: school.name, imageLocation: school.imageLocation})));
-      // Lets generate some students Randomly just to get the Database going eh?
-      const students = [];
-      for (let i = 0; i < 100; i++){
-        const tempS = {};
-        tempS.firstName = faker.name.firstName();
-        tempS.lastName = faker.name.lastName();
-        tempS.email = faker.internet.email();
-        tempS.GPA = Math.round(Math.random() * 40) / 10;
-        // Maybe assign student to school
-        let randomSchool = Math.floor(Math.random() * (schools.length + 1));
-        if (randomSchool < schools.length) tempS.schoolId = schools[randomSchool].id;
-        students.push(tempS);
-      }
-      await Promise.all(students.map( s=> Student.create(s)));
-    });
-  }
-  catch(e){
+  try {
+    await db.sync({ force: true });
+
+    const src = path.join(__dirname, 'seedData', 'schools.json');
+    let data = fs.readFileSync(src, 'utf8');
+    data = JSON.parse(data);
+
+    const schools = await Promise.all(
+      data.map(school =>
+        School.create({
+          name: school.name,
+          imageLocation: school.imageLocation,
+        })
+      )
+    );
+    // Lets generate some students Randomly just to get the Database going eh?
+    const students = [];
+    for (let i = 0; i < 100; i++) {
+      const tempS = {};
+      tempS.firstName = faker.name.firstName();
+      tempS.lastName = faker.name.lastName();
+      tempS.email = faker.internet.email();
+      tempS.GPA = Math.round(Math.random() * 40) / 10;
+      // Maybe assign student to school
+      let randomSchool = Math.floor(Math.random() * (schools.length + 1));
+      if (randomSchool < schools.length)
+        tempS.schoolId = schools[randomSchool].id;
+      students.push(tempS);
+    }
+    await Promise.all(students.map(s => Student.create(s)));
+  } catch (e) {
     console.log(e);
   }
 }
 
 syncAndSeed();
 
-module.exports = {Student, School, syncAndSeed, db};
+module.exports = { Student, School, syncAndSeed, db };
